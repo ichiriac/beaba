6615d74cd98aa0a684a2e49ff6ad778832d3a030
Ioan CHIRIAC <i.chiriac@yahoo.com>
2013-05-03 23:51:25 +0200

    fix 404

--- config/routes.php
+++ config/routes.php
@@ -21,24 +21,11 @@ return array(
             if ( empty( $parts[1] ) ) $parts[1] = 'index';
             $target = APP_PATH . '/controllers/' . strtolower($parts[0]) . '.php';
             $class = 'app\\controllers\\' . strtolower($parts[0]);
-            if ( !class_exists( $class, false ) ) {
-                if (
-                    !file_exists( $target )
-                ) {
-
-                    throw new \beaba\core\Exception(
-                        'Unable to find controller : ' . strtolower($parts[0]),
-                        404
-                    );
-                } else {
-                    include $target;
-                    if ( !class_exists( $class, false ) ) {
-                        throw new \beaba\core\Exception(
-                            'Unable to find class controller : ' . $class,
-                            404
-                        );
-                    }
-                }
+            if ( !class_exists( $class ) ) {
+                throw new \beaba\core\Exception(
+                    'Unable to find controller : ' . strtolower($parts[0]),
+                    404
+                );
             }
             return $class . '::' . strtolower($parts[1]);
         }
