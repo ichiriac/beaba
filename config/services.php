cd6104aa0fa58230c852144cf17c67dedd9d65c1
Ioan CHIRIAC <i.chiriac@yahoo.com>
2013-05-04 03:30:30 +0200

    adding debug options to view service

--- config/services.php
+++ config/services.php
@@ -9,7 +9,32 @@ return array(
     'errors'    => 'beaba\\services\\ErrorHandler',
     'assets'    => 'beaba\\services\\Assets',
     'storage'   => 'beaba\\services\\Storage',
-    'view'      => 'beaba\\services\\View',
+    'view'      => array(
+        'class' => 'beaba\\services\\View',
+        'options' => array(
+            'debug' => array(
+                'enabled' => false,
+                'view' => array(
+                    'enabled' => true,
+                    'border' => 'solid 1px #666666;',
+                    'text' => array(
+                        'size' => '9px',
+                        'color' => '#ffffff',
+                        'background' => '#333333'
+                    )
+                ),
+                'placeholder' => array(
+                    'enabled' => true,
+                    'border' => 'dotted 2px #663333;',
+                    'text' => array(
+                        'size' => '9px',
+                        'color' => '#ffffff',
+                        'background' => '#330000'
+                    )
+                )
+            )
+        )
+    ),
     'session'   => 'beaba\\services\\Session',
     'infos'     => 'beaba\\services\\Infos',
 );
