fbd7d719395dc3ba9c27df29c152c0a8a4851f91
Ioan CHIRIAC <i.chiriac@yahoo.com>
2013-05-04 03:44:43 +0200

    fixes, changing the page title

--- demo/default/controllers/index.php
+++ demo/default/controllers/index.php
@@ -5,10 +5,13 @@ use \beaba\core\Controller;
 class index extends Controller
 {
     public function index_action( $args ) {
-        return $this->getView()->push(
-            'content', 'controllers/index', array(
-                'text' => 'pass some dynamic data'
+        return $this->getView()
+            ->setTitle('Customized homepage title')
+            ->push(
+                'content', 'controllers/index', array(
+                    'text' => 'pass some dynamic data'
+                )
             )
-        );
+        ;
     }
 }
